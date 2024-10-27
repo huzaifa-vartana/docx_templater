3d731ec624831525ca40c8fc696dc38f75b3611b
huzaifa-vartana <huzaifa@vartana.com>
2024-10-27 16:11:31 -0400

    init

---
+++ lib/docx_templater.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "docx_templater/version"
+
+module DocxTemplater
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/docx_templater/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module DocxTemplater
+  VERSION = "0.1.0"
+end
